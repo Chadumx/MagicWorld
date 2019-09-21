7e235117d792a2cbfe5c613f251d89187fe1089f
Chadum <christopherhoussin@gmail.com>
2019-09-21 16:38:56 +0200

    creation de la methode de choix du type de guerrier. Premier jet.

--- src/com/christopher/MagicWorld/main.java
+++ src/com/christopher/MagicWorld/main.java
@@ -1,9 +1,30 @@
 package com.christopher.MagicWorld;
 
+import java.util.Scanner;
+
 public class main {
 
     public static void main(String[] args) {
 
+        Scanner clavier = new Scanner(System.in);
+        System.out.println("choix du ppersonnage :");
+        int choix;
+
+        do {
+            System.out.println("1: Guerrier\n2: Rôdeur\n3: Mage");
+            choix = clavier.nextInt();
+
+            if ( choix == 1){
+                System.out.println("guerrier");
+            } else if ( choix == 2){
+                System.out.println("rodeur");
+            } else if ( choix == 3) {
+                System.out.println("mage");
+            }else {
+                System.out.println("Veuillez choisir parmie les proposition donner !");
+            }
+
+        } while ( choix <= 0 || choix >= 4);
     }
 
 }
